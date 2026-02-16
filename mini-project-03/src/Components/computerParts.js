1edc3bf7198b6e901ecff995f15d16d273386a17
sannouki <t.ong25@ncf.edu>
2026-02-15 22:11:42 -0500

    updated images for item specs for each type of part

--- mini-project-03/src/Components/computerParts.js
+++ mini-project-03/src/Components/computerParts.js
@@ -1,60 +1,60 @@
 export const cpus = [
     {
-        name: "Intel Core i5-12600K", price: 100, img: "src/assets/cpu1.png"
+        name: "Intel Core i5-12600K", price: 100, img: "src/assets/cpu.jpeg"
     },
     {
-        name: "Intel Core i7-12700K", price: 200, img: "src/assets/cpu2.png"
+        name: "Intel Core i7-12700K", price: 200, img: "src/assets/cpu.jpeg"
     },
     {
-        name: "AMD Ryzen 5 5600X", price: 300, img: "src/assets/cpu3.png"
+        name: "AMD Ryzen 5 5600X", price: 300, img: "src/assets/cpu.jpeg"
     },
     {
-        name: "AMD Ryzen 7 5800X", price: 400, img: "src/assets/cpu4.png"
+        name: "AMD Ryzen 7 5800X", price: 400, img: "src/assets/cpu.jpeg"
     }
 ];
 
 export const gpus = [
     {
-        name: "NVIDIA GeForce RTX 4090", price: 500, img: "src/assets/cpu1.png"
+        name: "NVIDIA GeForce RTX 4090", price: 500, img: "src/assets/gpu.jpeg"
     },
     {
-        name: "AMD Radeon RX 7900 XTX", price: 600, img: "src/assets/cpu2.png"
+        name: "AMD Radeon RX 7900 XTX", price: 600, img: "src/assets/gpu.jpeg"
     },
     {
-        name: "NVIDIA GeForce RTX 4070 Ti", price: 700, img: "src/assets/cpu3.png"
+        name: "NVIDIA GeForce RTX 4070 Ti", price: 700, img: "src/assets/gpu.jpeg"
     },
     {
-        name: "AMD Radeon RX 7800 XT", price: 800, img: "src/assets/cpu4.png"
+        name: "AMD Radeon RX 7800 XT", price: 800, img: "src/assets/gpu.jpeg"
     }
 ];
 
 export const rams = [
     {
-        name: "Corsair Vengeance", price: 100.50, img: "src/assets/cpu1.png"
+        name: "Corsair Vengeance", price: 100.50, img: "src/assets/ram.jpeg"
     },
     {
-        name: "G.Skill Trident Z", price: 200.50, img: "src/assets/cpu2.png"
+        name: "G.Skill Trident Z", price: 200.50, img: "src/assets/ram.jpeg"
     },
     {
-        name: "Kingston FURY Beast", price: 300.99, img: "src/assets/cpu3.png"
+        name: "Kingston FURY Beast", price: 300.99, img: "src/assets/ram.jpeg"
     },
     {
-        name: "Crucial Ballistix", price: 400.99, img: "src/assets/cpu4.png"
+        name: "Crucial Ballistix", price: 400.99, img: "src/assets/ram.jpeg"
     }
 ];
 
 export const cases = [
     {
-        name: "Fractal Design Define 7", price: 150, img: "src/assets/cpu1.png"
+        name: "Fractal Design Define 7", price: 150, img: "src/assets/nzxt.jpeg"
     },
     {
-        name: "Corsair 4000D Airflow", price: 100, img: "src/assets/cpu2.png"
+        name: "Corsair 4000D Airflow", price: 100, img: "src/assets/nzxt.jpeg"
     },
     {
-        name: "Lian Li PC-O11 Dynamic", price: 200, img: "src/assets/cpu3.png"
+        name: "Lian Li PC-O11 Dynamic", price: 200, img: "src/assets/nzxt.jpeg"
     },
     {
-        name: "Fractal Design Meshify C", price: 120, img: "src/assets/cpu4.png"
+        name: "Fractal Design Meshify C", price: 120, img: "src/assets/nzxt.jpeg"
     }
 ];
 
